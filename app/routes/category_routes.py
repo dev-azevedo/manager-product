4dfa5059671f50fa68bf2753507f38b24f853c17
Jhonatan Azevedo <dev.azevedo@outlook.com>
2024-11-14 15:58:32 -0300

    CHANGE: documentation for swagger

--- app/routes/category_routes.py
+++ app/routes/category_routes.py
@@ -1,24 +1,25 @@
+from typing import List
 from fastapi import APIRouter, Depends, Response, status
 from sqlalchemy.orm import Session
-from app.schemas.category import Category
+from app.schemas.category import Category, CategoryOutput
 from app.routes.deps import get_db_session
 from app.use_cases.category import CategoryUseCases
 
 router = APIRouter(prefix="/categories", tags=["Category"])
 
-@router.post("/add")
+@router.post("/add", status_code=status.HTTP_201_CREATED, description="Add new category")
 def add_category(category: Category, db_session: Session = Depends(get_db_session)):
     uc = CategoryUseCases(db_session)
     uc.add_category(category)
     return Response(status_code=status.HTTP_201_CREATED)
 
-@router.get("/list")
+@router.get("/list", response_model=List[CategoryOutput], description="List categories")
 def list_categories(db_session: Session = Depends(get_db_session)):
     uc = CategoryUseCases(db_session)
     categories = uc.list_categories()
     return categories
 
-@router.delete("/delete/{id}")
+@router.delete("/delete/{id}", description="Delete category")
 def delete_category(id: int, db_session: Session = Depends(get_db_session)):
     uc = CategoryUseCases(db_session)
     uc.delete_category(id)
