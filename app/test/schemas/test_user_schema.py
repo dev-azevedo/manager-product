62b8667a0ad013b4200314cc8585a95e40d2b64e
Jhonatan Azevedo <dev.azevedo@outlook.com>
2024-11-14 17:27:33 -0300

    ADD: schema tokeb date

--- app/test/schemas/test_user_schema.py
+++ app/test/schemas/test_user_schema.py
@@ -1,5 +1,6 @@
 import pytest
-from app.schemas.user import User
+from datetime import datetime
+from app.schemas.user import User, TokenData
 
 def test_user_schema():
     user = User(username="jhow", password="jhowPass")
@@ -14,3 +15,11 @@ def test_user_schema_invalid_username():
         user = User(username="jhow#%!%", password="jhowPass")
     
     
+def test_token_date():
+    expire_at = datetime.now()
+    token_data = TokenData(access_token='token teste', expires_at=expire_at)
+    
+    assert token_data.dict() == {
+        "access_token": "token teste",
+        "expires_at": expire_at
+    }
