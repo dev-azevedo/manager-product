6e81f03f519faa2ff9f635347f42c4b683e2493e
Jhonatan Azevedo <dev.azevedo@outlook.com>
2024-11-19 10:25:56 -0300

    ADD: test and use_case user_login routes

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, Response, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from app.routes.deps import get_db_session
 from app.schemas.user import User
@@ -12,3 +13,13 @@ def register_user(user: User, db_session: Session = Depends(get_db_session)):
     uc = UserUseCases(db_session)
     uc.register_user(user)
     return Response(status_code=status.HTTP_201_CREATED) 
+
+@router.post("/login", status_code=status.HTTP_200_OK, description="Login user")
+def user_login(form_data: OAuth2PasswordRequestForm = Depends(), db_session: Session = Depends(get_db_session)):
+    uc = UserUseCases(db_session)
+    
+    user = User (username=form_data.username, password=form_data.password)
+    
+    token_data = uc.user_login(user=user, expires_in=60)
+    
+    return token_data
